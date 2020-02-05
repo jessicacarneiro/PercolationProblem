4521759424535b5297712a6857c09a594e90bda9
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2020-02-05 11:27:02 -0300

    Adding functions to convert site number 1 to n to coordinates (row,col).

--- src/Percolation.java
+++ src/Percolation.java
@@ -33,9 +33,40 @@ public class Percolation {
     public boolean percolates() {
     	return false;
     }
+    
+    /*
+     * Given a coordinate (ROW,COLUMN) returns the number corresponding
+     * to this site. Couting starts at site (0,0) = 1 and is incremented
+     * by one moving to left site (0,1) = 2, (0,2) = 3 and so forth.
+     * @param row site's row number
+     * @param col site's column number
+     * @param n size of the grid (n * n)
+     * @return the ith site number corresponding to the coordinate provided (ROW,COL)
+     */
+    private static int coordinatesToNumber(int row, int col, int n) {
+    	return n*row + (col+1);
+    }
+
+    /*
+     * Given a site number returns the number corresponding coordinate
+     * in the format (ROW,COLUMN) to this site. Couting starts at site
+     * (0,0) = 1 and is incremented by one moving to left site (0,1) = 2,
+     * (0,2) = 3 and so forth.
+     * @param siteNumber site's number
+     * @param n size of the grid (n * n)
+     * @return the coordinate (ROW,COL) corresponding to the site number
+     * provided
+     */
+    private static int[] numberToCoordinates(int siteNumber, int n) {
+        int[] coordinates = new int[2];
+        
+        coordinates[0] = (siteNumber % n == 0) ? (siteNumber/n) - 1 : (siteNumber / n); // row
+        coordinates[1] = (siteNumber % n == 0) ? n-1 : (siteNumber % n) - 1; // column
+
+        return coordinates;
+    }
 
     // test client (optional)
     public static void main(String[] args) {
-    	
     }
 }
