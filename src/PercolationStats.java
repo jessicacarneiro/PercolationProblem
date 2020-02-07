489d70039647e135db91ec3f26c20644f989ee5d
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2020-02-07 10:05:21 -0300

    Final untested experiment.

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -2,43 +2,65 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	
+
+	private int trials;
+	private int gridSize;
+	private double[] openSitesUntilPercolates;
+
 	// perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-    	if (n <= 0 || trials <= 0) {
-    		throw new IllegalArgumentException();
-    	}
-    }
+	public PercolationStats(int n, int trials) {
+		if (n <= 0 || trials <= 0) {
+			throw new IllegalArgumentException();
+		}
 
-    // sample mean of percolation threshold
-    public double mean() {
-    	return 0.0;
-    }
+		this.trials = trials;
+		this.gridSize = n;
+		this.openSitesUntilPercolates = new double[trials];
 
-    // sample standard deviation of percolation threshold
-    public double stddev() {
-    	return 0.0;
-    }
+		for (int i = 0; i < trials; i++) {
+			
+			Percolation p = new Percolation(n);
+			
+			while (!p.percolates()) {
+				int row = StdRandom.uniform(1, n + 1);
+				int col = StdRandom.uniform(1, n + 1);
 
-    // low endpoint of 95% confidence interval
-    public double confidenceLo() {
-    	return 0.0;
-    }
+				p.open(row, col);
+			}
+			
+			this.openSitesUntilPercolates[i] = (1.0*p.numberOfOpenSites())/(n*n);
+		}
+	}
 
-    // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-    	return 0.0;
-    }
+	// sample mean of percolation threshold
+	public double mean() {
+		return StdStats.mean(this.openSitesUntilPercolates);
+	}
 
-   // test client (see below)
-   public static void main(String[] args) {
-	   int gridSize = Integer.parseInt(args[0]);
-	   int trials = Integer.parseInt(args[1]);
-	   
-	   Percolation p = new Percolation(gridSize);
-	   
-	   for (int i = 0; i < trials; i++) {
-		   
-	   }
-   }
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		return StdStats.stddev(this.openSitesUntilPercolates);
+	}
+
+	// low endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return (this.mean() - 1.96 * this.stddev() / Math.sqrt(this.trials)); 
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return (this.mean() + 1.96 * this.stddev() / Math.sqrt(this.trials)); 
+	}
+
+	// test client (see below)
+	public static void main(String[] args) {
+		int gridSize = Integer.parseInt(args[0]);
+		int trials = Integer.parseInt(args[1]);
+
+		PercolationStats ps = new PercolationStats(gridSize, trials);
+		
+		System.out.println("mean\t\t\t= " + ps.mean());
+		System.out.println("stddev\t\t\t= " + ps.stddev());
+		System.out.println("95% confidence interval\t= " + "[" + ps.confidenceLo() + "," + ps.confidenceHi() + "]");
+	}
 }
